29d1b8da215f7ec4ab7f631a6f09a3c628dc21c8
kbisht84 <kanakbisht9@gmail.com>
2016-05-16 16:33:08 -0700

    Adding code for background process and updating UI

---
+++ app/src/androidTest/java/com/example/kanakb/sunshine/FullTestSuite.java
@@ -0,0 +1,20 @@
+package com.example.kanakb.sunshine;
+
+/**
+ * Created by KANAK#B on 5/9/2016.
+ */
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+
+    public FullTestSuite() {
+        super();
+    }
+}
